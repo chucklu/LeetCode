3a92a03f76539def1f501a61379ec22b04e36ea8
Chuck Lu <chuck.lu@qq.com>
2019-01-27 15:59:21 +0800

    add xunit

---
+++ LeetCode/MainTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public int NumComponents(ListNode head, int[] G)
+        {
+            int result = 0;
+            return result;
+        }
+    }
+
+    class MainTest
+    {
+    }
+}

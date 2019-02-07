41d48e8e29317a08b7cb65f74c683ff0ba0cc0ae
Chuck Lu <chuck.lu@qq.com>
2019-02-07 20:07:33 +0800

    Two Sum IV - Input is a BST https://leetcode.com/problems/two-sum-iv-input-is-a-bst/

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -1,63 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 using Xunit;
 using Xunit.Abstractions;
-using Xunit.Sdk;
 
 namespace LeetCode
 {
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+
     public class Test:BaseTest
     {
         public Test(ITestOutputHelper helper) : base(helper)
         {
         }
 
-        //https://leetcode.com/problems/valid-palindrome/
-        public bool IsPalindrome(string s)
+        //https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+        public bool FindTarget(TreeNode root, int k)
         {
-            string temp = "abcdefghijklmnopqrstuvwxyz0123456789";
-            s = s.ToLowerInvariant();
+            List<int> list = new List<int>();
+            IterateBinaryTree(root, list);
+            list.Sort();
+            var array = list.ToArray();
+
             int i = 0;
-            int j = s.Length - 1;
-            bool result = true;
+            int j = array.Length - 1;
             while (i < j)
             {
-                while (i < j)
+                if (array[i] + array[j] == k)
                 {
-                    if (!temp.Contains(s[i]))
-                    {
-                        i++;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                    return true;
                 }
-                while (i < j)
+                if (array[i] + array[j] > k)
                 {
-                    if (!temp.Contains(s[j]))
-                    {
-                        j--;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                if (s[i] == s[j])
-                {
-                    i++;
                     j--;
                 }
                 else
                 {
-                    result = false;
-                    break;
+                    i++;
                 }
             }
-            return result;
+            return false;
+        }
+
+        private static void IterateBinaryTree(TreeNode node, List<int> list)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            list.Add(node.val);
+
+            if (node.left != null)
+            {
+                IterateBinaryTree(node.left, list);
+            }
+            if (node.right != null)
+            {
+                IterateBinaryTree(node.right, list);
+            }
         }
 
         [Fact]
@@ -65,8 +75,19 @@ namespace LeetCode
         {
             try
             {
-                var str = "0P";
-                bool flag = IsPalindrome(str);
+                TreeNode root = new TreeNode(5);
+                root.left = new TreeNode(3);
+                root.right = new TreeNode(6);
+                root.left.left = new TreeNode(2);
+                root.left.right = new TreeNode(4);
+                root.right.right = new TreeNode(7);
+                bool flag = FindTarget(root, 9);
+                Output.WriteLine(flag.ToString());
+
+                root = new TreeNode(2);
+                root.left = new TreeNode(1);
+                root.right = new TreeNode(3);
+                flag = FindTarget(root, 4);
                 Output.WriteLine(flag.ToString());
             }
             catch (Exception ex)

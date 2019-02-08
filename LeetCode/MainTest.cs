6210af4ff819d0aafea1becdae031888f2bbfc60
Chuck Lu <chuck.lu@qq.com>
2019-02-08 20:29:53 +0800

    remove duplicates

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -5,83 +5,67 @@ using Xunit.Abstractions;
 
 namespace LeetCode
 {
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-
-        public TreeNode(int x)
-        {
-            val = x;
-        }
-    }
-
     public class Test:BaseTest
     {
         public Test(ITestOutputHelper helper) : base(helper)
         {
         }
 
-        //https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
-        public bool FindTarget(TreeNode root, int k)
+        //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+        public int RemoveDuplicates(int[] nums)
         {
-            List<int> list = new List<int>();
-            IterateBinaryTree(root, list);
-            list.Sort();
-            var array = list.ToArray();
+            if (nums == null)
+            {
+                return 0;
+            }
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            if (nums.Length == 1)
+            {
+                return 1;
+            }
 
             int i = 0;
-            int j = array.Length - 1;
-            while (i < j)
+            int j = i + 1;
+            while (j < nums.Length)
             {
-                if (array[i] + array[j] == k)
+                if (nums[i] == nums[j])
                 {
-                    return true;
-                }
-                if (array[i] + array[j] > k)
-                {
-                    j--;
+                    j++;
+                    if (j >= nums.Length)
+                    {
+                        break;
+                    }
                 }
                 else
                 {
                     i++;
+                    if (i >= nums.Length)
+                    {
+                        break;
+                    }
+                    if (j >= nums.Length)
+                    {
+                        break;
+                    }
+                    nums[i] = nums[j];
+                    j++;
                 }
             }
-            return false;
+
+            return i + 1;
         }
-
-        private static void IterateBinaryTree(TreeNode node, List<int> list)
-        {
-            if (node == null)
-            {
-                return;
-            }
-
-            list.Add(node.val);
-            IterateBinaryTree(node.left, list);
-            IterateBinaryTree(node.right, list);
-        }
-
+        
         [Fact]
         public void Test1()
         {
             try
             {
-                TreeNode root = new TreeNode(5);
-                root.left = new TreeNode(3);
-                root.right = new TreeNode(6);
-                root.left.left = new TreeNode(2);
-                root.left.right = new TreeNode(4);
-                root.right.right = new TreeNode(7);
-                bool flag = FindTarget(root, 9);
-                Output.WriteLine(flag.ToString());
-
-                root = new TreeNode(2);
-                root.left = new TreeNode(1);
-                root.right = new TreeNode(3);
-                flag = FindTarget(root, 4);
-                Output.WriteLine(flag.ToString());
+                int[] array = {1, 1,};
+                var result = RemoveDuplicates(array);
+                Output.WriteLine(result.ToString());
             }
             catch (Exception ex)
             {

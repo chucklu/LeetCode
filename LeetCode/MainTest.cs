f7965fffacacad40b8722a2a18f61c16470a0c14
Chuck Lu <chuck.lu@qq.com>
2019-02-25 19:07:39 +0800

    283. Move Zeroes

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -15,86 +15,38 @@ namespace LeetCode
         }
     }
 
-    //https://leetcode.com/problems/palindrome-linked-list/
-    //https://www.geeksforgeeks.org/function-to-check-if-a-singly-linked-list-is-palindrome/
+    //https://leetcode.com/problems/move-zeroes/
     public class Test : BaseTest
     {
         public Test(ITestOutputHelper helper) : base(helper)
         {
         }
 
-        public bool IsPalindrome(ListNode head)
+        public void MoveZeroes(int[] nums)
         {
-            if (head?.next == null)
+            if (nums == null)
             {
-                return true;
+                return;
             }
-            var slow = head;
-            var fast = head;
-            ListNode firstHead = head;
-            ListNode secondHead;
-            ListNode slowPrev = null;
-            // Get the middle of the list.
-            // Move slow by 1 and fast by 2
-            // slow will have the middle mode
-            while (fast?.next != null)
+            int i = 0;
+            int length = nums.Length - 1;
+            int counter = 0;
+            while (i < length)
             {
-                fast = fast.next.next;
-                //We need previous of the slow_ptr for linked lists  with odd elements
-                slowPrev = slow;
-                slow = slow.next;
-            }
-            if (slowPrev != null)
-            {
-                slowPrev.next = null;
-            }
-
-            if (fast != null)
-            {
-                //not null for odd elements
-                secondHead = slow.next;
-            }
-            else
-            {
-                secondHead = slow;
-            }
-
-            secondHead = ReverseList(secondHead);
-            bool flag = true;
-            while (firstHead != null && secondHead != null)
-            {
-                if (firstHead.val != secondHead.val)
+                if (nums[i] != 0)
                 {
-                    flag = false;
-                    break;
+                    nums[counter] = nums[i];
+                    counter++;
                 }
-                else
-                {
-                    firstHead = firstHead.next;
-                    secondHead = secondHead.next;
-                }
+
+                i++;
             }
 
-            if (firstHead != null || secondHead != null)
+            while (counter < length)
             {
-                flag = false;
+                nums[counter] = 0;
+                counter++;
             }
-
-            return flag;
-        }
-
-        public ListNode ReverseList(ListNode head)
-        {
-            ListNode prev = null;
-            ListNode curr = head;
-            while (curr != null)
-            {
-                ListNode nextTemp = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = nextTemp;
-            }
-            return prev;
         }
 
         [Fact]

6904c446bce60807e46e175f364ff3bc12816f56
Chuck Lu <chuck.lu@qq.com>
2019-04-12 12:45:01 +0800

    226. Invert Binary Tree

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -31,10 +31,18 @@ namespace LeetCode
             {
                 root.right = InvertTree(left);
             }
+            else
+            {
+                root.right = null;
+            }
 
             if (right != null)
             {
                 root.left = InvertTree(right);
+            }
+            else
+            {
+                root.left = null;
             }
 
             return root;
@@ -83,26 +91,12 @@ namespace LeetCode
         {
             try
             {
-                TreeNode node1 = new TreeNode(5);
-                TreeNode node2 = new TreeNode(4);
-                TreeNode node3 = new TreeNode(8);
+                TreeNode node1 = new TreeNode(1);
+                TreeNode node2 = new TreeNode(2);
                 node1.left = node2;
-                node1.right = node3;
 
-                TreeNode node4 = new TreeNode(11);
-                TreeNode node5 = new TreeNode(13);
-                TreeNode node6 = new TreeNode(4);
-                node2.left = node4;
-                node3.left = node5;
-                node3.right = node6;
-
-
-                TreeNode node7 = new TreeNode(7);
-                TreeNode node8 = new TreeNode(2);
-                TreeNode node9 = new TreeNode(1);
-                node4.left = node7;
-                node4.right = node8;
-                node6.right = node9;
+                var node = InvertTree(node1);
+                WriteTreeNode(node);
 
             }
             catch (Exception ex)

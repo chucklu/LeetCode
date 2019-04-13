cb07b837e4c1620ebaeb3ddcbc50d94244bf9ef6
Chuck Lu <chuck.lu@qq.com>
2019-04-13 12:45:51 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -6,46 +6,30 @@ using Xunit.Abstractions;
 namespace LeetCode
 {
 
-    //https://leetcode.com/problems/invert-binary-tree/
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
     public class MainTest : BaseTest
     {
         public MainTest(ITestOutputHelper helper) : base(helper)
         {
         }
 
-        public TreeNode InvertTree(TreeNode root)
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
-            if (root == null)
+            TreeNode node;
+            if (root.val > p.val && root.val > q.val)
             {
-                return null;
+                node = LowestCommonAncestor(root.left, p, q);
             }
-
-            var left = root.left;
-            var right = root.right;
-            if (left == null && right == null)
+            else if (root.val < p.val && root.val < q.val)
             {
-                return root;
-            }
-
-            if (left != null)
-            {
-                root.right = InvertTree(left);
+                node = LowestCommonAncestor(root.right, p, q);
             }
             else
             {
-                root.right = null;
+                node = root;
             }
 
-            if (right != null)
-            {
-                root.left = InvertTree(right);
-            }
-            else
-            {
-                root.left = null;
-            }
-
-            return root;
+            return node;
         }
 
         private void WriteTreeNode(TreeNode node)
@@ -91,12 +75,29 @@ namespace LeetCode
         {
             try
             {
-                TreeNode node1 = new TreeNode(1);
-                TreeNode node2 = new TreeNode(2);
-                node1.left = node2;
+                TreeNode node1 = new TreeNode(6);
 
-                var node = InvertTree(node1);
-                WriteTreeNode(node);
+                TreeNode node21 = new TreeNode(2);
+                TreeNode node22 = new TreeNode(8);
+                node1.left = node21;
+                node1.right = node22;
+
+                TreeNode node31 = new TreeNode(0);
+                TreeNode node32 = new TreeNode(4);
+                node21.left = node31;
+                node21.right = node32;
+
+                TreeNode node33 = new TreeNode(7);
+                TreeNode node34 = new TreeNode(9);
+                node22.left = node33;
+                node22.right = node34;
+
+                TreeNode node43 = new TreeNode(3);
+                TreeNode node44 = new TreeNode(5);
+                node32.left = node43;
+                node32.right = node44;
+
+                LowestCommonAncestor(node1, node21, node22);
 
             }
             catch (Exception ex)

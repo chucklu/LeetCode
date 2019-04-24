7bca6016f00928f208529c1a69218dd822d19016
Chuck Lu <chuck.lu@qq.com>
2019-04-24 12:43:31 +0800

    add test case

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -94,18 +94,34 @@ namespace LeetCode
         {
             try
             {
-                TreeNode node1 = new TreeNode(3);
-                TreeNode node2 = new TreeNode(9);
-                TreeNode node3 = new TreeNode(20);
+                TreeNode node1 = new TreeNode(5);
+                TreeNode node2 = new TreeNode(4);
+                TreeNode node3 = new TreeNode(8);
                 node1.left = node2;
                 node1.right = node3;
 
-                TreeNode node4 = new TreeNode(15);
-                TreeNode node5 = new TreeNode(7);
-                node3.left = node4;
-                node3.right = node5;
-                //int tempSum = SumOfLeftLeaves(node1);
-                //Output.WriteLine(tempSum.ToString());
+                TreeNode node4 = new TreeNode(11);
+                TreeNode node5 = new TreeNode(13);
+                TreeNode node6 = new TreeNode(4);
+                node2.left = node4;
+                node3.left = node5;
+                node3.right = node6;
+
+                TreeNode node7 = new TreeNode(7);
+                TreeNode node8 = new TreeNode(2);
+                TreeNode node9 = new TreeNode(5);
+                TreeNode node10 = new TreeNode(1);
+                node4.left = node7;
+                node4.right = node8;
+                node6.left = node9;
+                node6.right = node10;
+
+                var tempList = PathSum(node1, 22);
+                foreach (var item in tempList)
+                {
+                    string str = string.Join("->", item);
+                    Output.WriteLine(str);
+                }
             }
             catch (Exception ex)
             {

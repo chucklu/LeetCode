1e40a7735e1cacb4f4a51ef1374becdd6c03a13e
Chuck Lu <chuck.lu@qq.com>
2019-04-28 12:33:42 +0800

    set the relation between tree nodes

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -90,9 +90,13 @@ namespace LeetCode
             {
                 TreeNode node1 = new TreeNode(1);
                 TreeNode node2 = new TreeNode(2);
+                node1.right = node2;
                 TreeNode node3 = new TreeNode(3);
+                node2.right = node3;
                 TreeNode node4 = new TreeNode(4);
+                node3.right = node4;
                 TreeNode node5 = new TreeNode(5);
+                node4.right = node5;
                 int count = PathSum(node1, 3);
                 Output.WriteLine(count.ToString());
             }

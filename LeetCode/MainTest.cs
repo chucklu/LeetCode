8d9513c822c087d6ab9bb7f2593ba11c0b46cecf
Chuck Lu <chuck.lu@qq.com>
2019-05-16 19:41:05 +0800

    add test case

--- LeetCode/MainTest.cs
+++ LeetCode/MainTest.cs
@@ -93,6 +93,25 @@ namespace LeetCode
         {
             try
             {
+                ListNode node1 = new ListNode(4);
+                ListNode node2 = new ListNode(1);
+                node1.next = node2;
+                ListNode node3 = new ListNode(8);
+                node2.next = node3;
+                ListNode node4 = new ListNode(4);
+                node3.next = node4;
+                ListNode node5 = new ListNode(5);
+                node4.next = node5;
+
+                ListNode node6 = new ListNode(5);
+                ListNode node7 = new ListNode(0);
+                node6.next = node7;
+                ListNode node8 = new ListNode(1);
+                node7.next = node8;
+                node8.next = node3;
+
+                var resultNode = GetIntersectionNode(node1, node6);
+                Output.WriteLine(resultNode.val.ToString());
             }
             catch (Exception ex)
             {

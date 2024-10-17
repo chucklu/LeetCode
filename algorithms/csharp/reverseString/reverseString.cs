1560677f08c229002948d835aaa9cfedf24590ad
Chuck LU <chucklu2015@outlook.com>
2024-10-17 15:11:11 +0800

    rename variables

--- algorithms/csharp/reverseString/reverseString.cs
+++ algorithms/csharp/reverseString/reverseString.cs
@@ -4,15 +4,16 @@ public class Solution
 {
     public void ReverseString(char[] s)
     {
-        int i = 0;
-        int j = s.Length - 1;
-        while (i < j)
+        int left = 0;
+        int right = s.Length - 1;
+        while (left < right)
         {
-            var temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+			
+            left++;
+            right--;
         }
     }
 }

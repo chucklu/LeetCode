bb849ff15245bc5a33d46e2986a21c92541acf7c
Chuck LU <chucklu2015@outlook.com>
2024-10-17 14:44:32 +0800

    344. Reverse String with old solution

---
+++ algorithms/csharp/reverseString/reverseString.cs
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/reverse-string/
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int i = 0;
+        int j = s.Length - 1;
+        while (i < j)
+        {
+            var temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
